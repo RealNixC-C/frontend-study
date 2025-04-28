c9ba8f46bf49a8e168ee58f8f8ecae94999b734a
RealNixC-C <john7622@naver.com>
2025-04-28 18:04:25 +0900

    16일차 수업 끝 집가서 꼭 복습할것

---
+++ Homework/01_Homework/main.js
@@ -0,0 +1,11 @@
+const btnEl = document.querySelector('.btn');
+const imageEl = document.querySelector('img');
+
+console.log(btnEl);
+console.log(imageEl);
+
+
+btnEl.addEventListener('click', function () {
+  imageEl.width =
+  imageEl.height
+});
